049ed061d6507a41246d7391aa0b6b6ab85d54c4
v0agent <it+v0agent@vercel.com>
2026-05-28 19:52:18 +0000

    feat: rename "Firma/Fartøy" to "Fartøy/Rederi" in contact form and route

--- components/contact.tsx
+++ components/contact.tsx
@@ -116,7 +116,7 @@ export default function Contact() {
               <input name="phone" type="tel" placeholder="+47 000 00 000" />
             </div>
             <div className="fg">
-              <label>Firma/Fartøy</label>
+              <label>Fartøy/Rederi</label>
               <input name="company" type="text" placeholder="Fartøy/Rederi" />
             </div>
           </div>
